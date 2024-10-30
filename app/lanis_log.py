9bf4fd2b994784e4015844cd11df90ab672d226d
dewenni <kontakt@toensjost.de>
2024-10-30 21:02:44 +0100

    add timestamp for logging

--- app/lanis_log.py
+++ app/lanis_log.py
@@ -2,6 +2,13 @@ import logging
 import sys
 
 def setup_logger():
+
+    logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s - %(name)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+    
     logger = logging.getLogger('LanisAPP')
     logger.setLevel(logging.INFO)
 
